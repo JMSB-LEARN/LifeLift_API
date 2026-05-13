ff021292b8cf687fea4391378e3cbfe64d8040f2
JMSBD <jmarquess04@iesalbarregas.es>
2026-05-13 16:36:06 +0200

    Fix 2

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ if (!secretKey) {
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'https://life-lift.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
